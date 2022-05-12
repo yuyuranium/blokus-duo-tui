c34fbc97c6074ea8a1dba10c93a26ae4c79085eb
choucl <clc25600@gmail.com>
2022-05-13 00:36:16 +0800

    Modify tile candidate render with blokus tile definitions

--- inc/render.h
+++ inc/render.h
@@ -3,10 +3,8 @@
 
 #include "blokus.h"
 
-extern const coord_t TILES_CURS[SHAPE_Z + 1][5];
+extern const coord_t TILES_CURS[SHAPE_Z + 1];
 extern const coord_t ALPHA_CURS[SHAPE_Z + 1];
-extern const char ALPHAS[SHAPE_Z + 1];
-extern const char BLKCNT[SHAPE_Z + 1];
 
 int modify_board(gcb_t *gcb);
 int modify_tiles(gcb_t *gcb, int player);
--- src/render.c
+++ src/render.c
@@ -1,138 +1,50 @@
+#include "blokus.h"
 #include "render.h"
 #include <ncurses.h>
 
-const coord_t TILE_CURS[SHAPE_Z + 1][5] = {
-    {   // SHAPE_O 
-        {.y = 15, .x = 68},
-    },
-    {   // SHAPE_R 
-        {.y = 15, .x = 60},
-        {.y = 15, .x = 62},
-    },
-    {   // SHAPE_M 
-        {.y = 15, .x = 51},
-        {.y = 15, .x = 53},
-        {.y = 15, .x = 55},
-    },
-    {   // SHAPE_A 
-        {.y = 14, .x = 45},
-        {.y = 14, .x = 47},
-        {.y = 15, .x = 47},
-    },
-    {   // SHAPE_E 
-        {.y = 2, .x = 33},
-        {.y = 2, .x = 35},
-        {.y = 2, .x = 37},
-        {.y = 2, .x = 39},
-    },
-    {   // SHAPE_J 
-        {.y = 5, .x = 35},
-        {.y = 6, .x = 35},
-        {.y = 7, .x = 35},
-        {.y = 7, .x = 33},
-    },
-    {   // SHAPE_S 
-        {.y = 5, .x = 38},
-        {.y = 6, .x = 38},
-        {.y = 6, .x = 40},
-        {.y = 7, .x = 40},
-    },
-    {   // SHAPE_Q 
-        {.y = 10, .x = 38},
-        {.y = 11, .x = 38},
-        {.y = 10, .x = 40},
-        {.y = 11, .x = 40},
-    },
-    {   // SHAPE_H 
-        {.y = 10, .x = 33},
-        {.y = 11, .x = 35},
-        {.y = 11, .x = 33},
-        {.y = 12, .x = 33},
-    },
-    {   // SHAPE_F 
-        {.y = 10, .x = 65},
-        {.y = 10, .x = 67},
-        {.y = 11, .x = 69},
-        {.y = 11, .x = 67},
-        {.y = 12, .x = 67},
-    },
-    {   // SHAPE_I 
-        {.y = 15, .x = 33},
-        {.y = 15, .x = 35},
-        {.y = 15, .x = 37},
-        {.y = 15, .x = 39},
-        {.y = 15, .x = 41},
-    },
-    {   // SHAPE_L 
-        {.y = 1, .x = 48},
-        {.y = 2, .x = 42},
-        {.y = 2, .x = 44},
-        {.y = 2, .x = 46},
-        {.y = 2, .x = 48},
-    },
-    {   // SHAPE_N 
-        {.y = 2, .x = 62},
-        {.y = 2, .x = 64},
-        {.y = 2, .x = 66},
-        {.y = 1, .x = 66},
-        {.y = 1, .x = 68},
-    },
-    {   // SHAPE_P 
-        {.y = 5, .x = 44},
-        {.y = 5, .x = 46},
-        {.y = 6, .x = 44},
-        {.y = 6, .x = 46},
-        {.y = 7, .x = 44},
-    },
-    {   // SHAPE_T 
-        {.y = 10, .x = 58},
-        {.y = 10, .x = 60},
-        {.y = 10, .x = 62},
-        {.y = 11, .x = 60},
-        {.y = 12, .x = 60},
-    },
-    {   // SHAPE_U 
-        {.y = 10, .x = 44},
-        {.y = 11, .x = 44},
-        {.y = 11, .x = 46},
-        {.y = 11, .x = 48},
-        {.y = 10, .x = 48},
-    },
-    {   // SHAPE_V 
-        {.y = 5, .x = 50},
-        {.y = 6, .x = 50},
-        {.y = 7, .x = 50},
-        {.y = 7, .x = 52},
-        {.y = 7, .x = 54},
-    },
-    {   // SHAPE_W 
-        {.y = 10, .x = 52},
-        {.y = 11, .x = 52},
-        {.y = 11, .x = 54},
-        {.y = 12, .x = 54},
-        {.y = 12, .x = 56},
-    },
-    {   // SHAPE_X 
-        {.y = 6, .x = 64},
-        {.y = 6, .x = 66},
-        {.y = 6, .x = 68},
-        {.y = 5, .x = 66},
-        {.y = 7, .x = 66},
-    },
-    {   // SHAPE_Y 
-        {.y = 2, .x = 52},
-        {.y = 2, .x = 54},
-        {.y = 2, .x = 56},
-        {.y = 2, .x = 58},
-        {.y = 1, .x = 56},
-    },
-    {   // SHAPE_Z 
-        {.y = 5, .x = 57},
-        {.y = 5, .x = 59},
-        {.y = 6, .x = 59},
-        {.y = 7, .x = 59},
-        {.y = 7, .x = 61},
-    },
+const coord_t TILE_CURS[SHAPE_Z + 1] = {
+     // SHAPE_O 
+     {.y = 15, .x = 68},
+     // SHAPE_R 
+     {.y = 15, .x = 60},
+     // SHAPE_M 
+     {.y = 15, .x = 53},
+     // SHAPE_A 
+     {.y = 14, .x = 47},
+     // SHAPE_E 
+     {.y = 2, .x = 35},
+     // SHAPE_J 
+     {.y = 7, .x = 35},
+     // SHAPE_S 
+     {.y = 6, .x = 40},
+     // SHAPE_Q 
+     {.y = 10, .x = 38},
+     // SHAPE_H 
+     {.y = 11, .x = 33},
+     // SHAPE_F 
+     {.y = 11, .x = 67},
+     // SHAPE_I 
+     {.y = 15, .x = 37},
+     // SHAPE_L 
+     {.y = 2, .x = 48},
+     // SHAPE_N 
+     {.y = 2, .x = 66},
+     // SHAPE_P 
+     {.y = 6, .x = 44},
+     // SHAPE_T 
+     {.y = 10, .x = 60},
+     // SHAPE_U 
+     {.y = 11, .x = 46},
+     // SHAPE_V 
+     {.y = 7, .x = 50},
+     // SHAPE_W 
+     {.y = 11, .x = 54},
+     // SHAPE_X 
+     {.y = 6, .x = 66},
+     // SHAPE_Y 
+     {.y = 2, .x = 56},
+     // SHAPE_Z 
+     {.y = 6, .x = 59},
 };
 
 const coord_t ALPHA_CURS[SHAPE_Z + 1] = {
@@ -179,12 +91,7 @@ const coord_t ALPHA_CURS[SHAPE_Z + 1] = {
     // SHAPE_Z
     {.y = 4, .x = 59},
 };
-const char ALPHAS[SHAPE_Z + 1] = {
-    'M', 'D', 'R', 'A', 'E', 'J', 'S', 'Q', 'H', 'F', 'I', 'L', 'N', 'P', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' 
-};
-const char BLKCNT[SHAPE_Z + 1] = {
-    1, 2, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5
-};
+
 int modify_board(gcb_t *gcb)
 {
     mvprintw(1, 0, "  1 2 3 4 5 6 7 8 9 a b c d e  │\n");
@@ -203,15 +110,18 @@ int modify_tiles(gcb_t *gcb, int player)
 {
     for (int i = 0; i < SHAPE_Z + 1; ++i) {
         if (gcb->hand[player][i]) {
-            mvprintw(ALPHA_CURS[i].y, ALPHA_CURS[i].x, "%c", ALPHAS[i]);
-            for (int j = 0; j < BLKCNT[i]; ++j) {
-                mvprintw(TILE_CURS[i][j].y, TILE_CURS[i][j].x, "%s",
-                        (player)? "□" : "■");
+            mvprintw(ALPHA_CURS[i].y, ALPHA_CURS[i].x, "%c", TILE[i].alpha);
+            for (int j = 0; j < TILE[i].blk_cnt; ++j) {
+                mvprintw(TILE_CURS[i].y + TILE[i].blks[j].y, 
+                         TILE_CURS[i].x + TILE[i].blks[j].x * 2,
+                         "%s", (player)? "□" : "■");
             }
         } else {
             mvprintw(ALPHA_CURS[i].y, ALPHA_CURS[i].x, " ");
-            for (int j = 0; j < BLKCNT[i]; ++j) {
-                mvprintw(TILE_CURS[i][j].y, TILE_CURS[i][j].x, " ");
+            for (int j = 0; j < TILE[i].blk_cnt; ++j) {
+                mvprintw(TILE_CURS[i].y + TILE[i].blks[j].y, 
+                         TILE_CURS[i].x + TILE[i].blks[j].x * 2,
+                         " ");
             }
         }
     }
