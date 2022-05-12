cbb7b0d63ad9c78152b5d87c55498e22153503f7
yuyuranium <e24076459@gs.ncku.edu.tw>
2022-05-12 10:20:24 +0800

    Add rot_cnt and can_mir attributes

--- src/blokus.c
+++ src/blokus.c
@@ -39,6 +39,8 @@ int test_place(gcb_t *gcb, tile_t *tile, coord_t coord)
     } else {
         for (int i = 0; i < tile->blk_cnt; ++i) {
             int y = coord.y + tile->blks[i].y, x = coord.x + tile->blks[i].x;
+
+            // Check if within map
             if (within_map(y, x)) {
                 // Overlapping
                 if (gcb->map[y][x] != -1)
@@ -59,7 +61,6 @@ int test_place(gcb_t *gcb, tile_t *tile, coord_t coord)
                     if (within_map(cy, cx) && gcb->map[cy][cx] == p)
                         valid = 1;
                 }
-
             } else {
                 return -1;
             }
@@ -72,7 +73,7 @@ int test_place(gcb_t *gcb, tile_t *tile, coord_t coord)
 tile_t *make_tile(shape_t shape)
 {
     tile_t *tile = malloc(sizeof(tile_t));
-    int blk_cnt;
+    int blk_cnt, can_mir = 0, rot_cnt = 0;
     coord_t *blks;
 
     if (shape <= SHAPE_O) {
@@ -86,6 +87,7 @@ tile_t *make_tile(shape_t shape)
         blks = malloc(blk_cnt * sizeof(coord_t));
         blks[0].y = 0; blks[0].x = 0;
         blks[1].y = 0; blks[1].x = 1;
+        rot_cnt = 1;
     } else if (shape <= SHAPE_A) {
         // trominoes
         blk_cnt = 3;
@@ -95,10 +97,12 @@ tile_t *make_tile(shape_t shape)
         case SHAPE_M:
             blks[1].y = 0; blks[1].x = -1;
             blks[2].y = 0; blks[2].x = 1;
+            rot_cnt = 1;
             break;
         case SHAPE_A:
             blks[1].y = 0; blks[1].x = -1;
             blks[2].y = 1; blks[2].x = 0;
+            rot_cnt = 3;
             break;
         default:
             ;
@@ -113,16 +117,21 @@ tile_t *make_tile(shape_t shape)
             blks[1].y = 0; blks[1].x = -1;
             blks[2].y = 0; blks[2].x = 1;
             blks[3].y = 0; blks[3].x = 2;
+            rot_cnt = 1;
             break;
         case SHAPE_J:
             blks[1].y = -2; blks[1].x = 0;
             blks[2].y = -1; blks[2].x = 0;
             blks[3].y = 0; blks[3].x = -1;
+            rot_cnt = 3;
+            can_mir = 1;
             break;
         case SHAPE_S:
             blks[1].y = -1; blks[1].x = -1;
             blks[2].y = 0; blks[2].x = -1;
             blks[3].y = 1; blks[3].x = 0;
+            rot_cnt = 1;
+            can_mir = 1;
             break;
         case SHAPE_Q:
             blks[1].y = 0; blks[1].x = 1;
@@ -133,6 +142,7 @@ tile_t *make_tile(shape_t shape)
             blks[1].y = -1; blks[1].x = 0;
             blks[2].y = 0; blks[2].x = 1;
             blks[3].y = 1; blks[3].x = 0;
+            rot_cnt = 3;
             break;
         default:
             ;
@@ -148,54 +158,67 @@ tile_t *make_tile(shape_t shape)
             blks[2].y = -1; blks[2].x = 0;
             blks[3].y = 0; blks[3].x = 1;
             blks[4].y = 1; blks[4].x = 0;
+            rot_cnt = 3;
+            can_mir = 1;
             break;
         case SHAPE_I:
             blks[1].y = 0; blks[1].x = -2;
             blks[2].y = 0; blks[2].x = -1;
             blks[3].y = 0; blks[3].x = 1;
             blks[4].y = 0; blks[4].x = 2;
+            rot_cnt = 1;
             break;
         case SHAPE_L:
             blks[1].y = -1; blks[1].x = 0;
             blks[2].y = 0; blks[2].x = -3;
             blks[3].y = 0; blks[3].x = -2;
             blks[4].y = 0; blks[4].x = -1;
+            rot_cnt = 3;
+            can_mir = 1;
             break;
         case SHAPE_N:
             blks[1].y = -1; blks[1].x = 0;
             blks[2].y = -1; blks[2].x = 1;
             blks[3].y = 0; blks[3].x = -2;
             blks[4].y = 0; blks[4].x = -1;
+            rot_cnt = 3;
+            can_mir = 1;
             break;
         case SHAPE_P:
             blks[1].y = -1; blks[1].x = 0;
             blks[2].y = -1; blks[2].x = 1;
             blks[3].y = 0; blks[3].x = 1;
             blks[4].y = 1; blks[4].x = 0;
+            rot_cnt = 3;
+            can_mir = 1;
             break;
         case SHAPE_T:
             blks[1].y = 0; blks[1].x = -1;
             blks[2].y = 0; blks[2].x = 1;
             blks[3].y = 1; blks[3].x = 0;
             blks[4].y = 2; blks[4].x = 0;
+            rot_cnt = 3;
             break;
         case SHAPE_U:
             blks[1].y = -1; blks[1].x = -1;
             blks[2].y = -1; blks[2].x = 1;
             blks[3].y = 0; blks[3].x = -1;
             blks[4].y = 0; blks[4].x = 1;
+            rot_cnt = 3;
             break;
         case SHAPE_V:
             blks[1].y = -1; blks[1].x = -1;
             blks[2].y = -1; blks[2].x = 0;
             blks[3].y = 0; blks[3].x = 1;
             blks[4].y = 1; blks[4].x = 0;
+            rot_cnt = 3;
             break;
         case SHAPE_W:
             blks[1].y = -2; blks[1].x = 0;
             blks[2].y = -1; blks[2].x = 0;
             blks[3].y = 0; blks[3].x = 1;
             blks[4].y = 0; blks[4].x = 2;
+            rot_cnt = 3;
             break;
         case SHAPE_X:
             blks[1].y = -1; blks[1].x = 0;
@@ -208,12 +231,16 @@ tile_t *make_tile(shape_t shape)
             blks[2].y = 0; blks[2].x = -2;
             blks[3].y = 0; blks[3].x = -1;
             blks[4].y = 0; blks[4].x = 1;
+            rot_cnt = 3;
+            can_mir = 1;
             break;
         case SHAPE_Z:
             blks[1].y = -1; blks[1].x = -1;
             blks[2].y = -1; blks[2].x = 0;
             blks[3].y = 1; blks[3].x = 0;
             blks[4].y = 1; blks[4].x = 1;
+            rot_cnt = 1;
+            can_mir = 1;
             break;
         default:
             ;
@@ -225,6 +252,8 @@ tile_t *make_tile(shape_t shape)
 
     tile->blk_cnt = blk_cnt;
     tile->blks = blks;
+    tile->can_mir = can_mir;
+    tile->rot_cnt = rot_cnt;
     return tile;
 }
 
