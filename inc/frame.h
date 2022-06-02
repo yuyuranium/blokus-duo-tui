32d773a4925104cfa04f5adf3c849a52cae4cc82
choucl <clc25600@gmail.com>
2022-06-02 16:18:43 +0800

    Add frame.h

---
+++ inc/frame.h
@@ -0,0 +1,22 @@
+#ifndef _FRAME_H
+#define _FRAME_H
+
+/*
+   |   0    |    1   | 2 - 19 |
+   | opcode | status |  code  |
+*/
+
+enum OPCODE {
+    REQ_PAIR,
+    PAIRED,
+    REQ_TURN,
+    REQ_STATUS,
+    RES,
+    REQ_PLACE
+};
+
+#define STATUS_INV -1
+#define STATUS_OK 1
+#define STATUS_FAILED 0
+
+#endif
