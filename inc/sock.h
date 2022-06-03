d7a0ed6024b47eb7e21f06b0dd9a933367e664a9
choucl <clc25600@gmail.com>
2022-06-03 11:03:52 +0800

    Add TURN op and add resend timeout

--- inc/sock.h
+++ inc/sock.h
@@ -1,6 +1,8 @@
 #ifndef _SOCK_H
 #define _SOCK_H
 
+#define TIMEOUT 10000
+
 int open_clientfd(char *hostname, char *port) __attribute__((unused));
 int open_listenfd(char *port) __attribute__((unused));
 
