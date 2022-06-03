e6d14b92748d8be6fb362ef7ca930f9e5af0ebd1
yuyuranium <e24076459@gs.ncku.edu.tw>
2022-06-03 15:09:00 +0800

    Create thread when client connected
    I modified CC to clang since in mac only this can compile correctly.

--- src/server.c
+++ src/server.c
@@ -5,23 +5,51 @@
 #include <pthread.h>
 #include "sock.h"
 
+static pthread_mutex_t mutex;
+
+static void die(char *msg)
+{
+    printf("%s\n", msg);
+    exit(-1);
+}
+
+static void *serve(void *argp)
+{
+    printf("hello");
+    return NULL;
+}
+
 int main(int argc, char *argv[])
 {
     char *server_port = NULL;
     --argc; ++argv;
     if (argc > 0 && **argv == '-' && (*argv)[1] == 'p') {
         --argc; ++argv;
-        if (argc < 1) {
-            printf("error: No port number provided\n");
-            return -1;
-        }
+        if (argc < 1)
+            die("error: No port number provided\n");
+
         server_port = malloc(strlen(*argv) + 1);
         strncpy(server_port, *argv, strlen(*argv));
         --argc; ++argv;
+
+        if (argc > 0)
+            die("error: too many arguments");
+    } else {
+        die("usage: servr -p server_port\n");
     }
-    if (argc < 1) {
-        printf("usage: servr -p server_port");
-        return -1;
+
+    int listenfd __attribute__((unused)) = open_listenfd(server_port);
+    printf("listening on the port %s\n", server_port);
+
+    pthread_mutex_init(&mutex, NULL);
+    while (1) {
+        struct sockaddr client_info;
+        socklen_t addrlen;
+        long clientfd = accept(listenfd, &client_info, &addrlen);
+        pthread_t thread;
+        pthread_create(&thread, NULL, serve, (void *)clientfd);
+        printf("info: thread %ld created, serving connection fd %ld\n",
+               (long)thread, clientfd);
     }
 
     return 0;
